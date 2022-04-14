61747bb78b08c9d962fbd91a395812c4fbe83d26
estartsev <estartsev@phoenixit.ru>
2022-04-14 13:04:19 +0300

    feat(auth-integration): Регистрация и авторизация

--- frontend/src/components/auth/RegisterForm.vue
+++ frontend/src/components/auth/RegisterForm.vue
@@ -1,39 +1,63 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
 import InputText from 'primevue/inputtext'
 import Button from 'primevue/button'
+import type { InferType } from 'yup'
+import { object, string, ref as yupRef } from 'yup'
+import { useField, useForm } from 'vee-validate'
+import { useRouter } from 'vue-router'
 import { registerUser } from '@api/auth/auth'
 
-const registerData = reactive({
-  email: undefined,
-  username: 'test_user',
-  password: undefined,
-  passwordConfirm: undefined,
+const schema = object({
+  email: string().email('Введите корректный email').required('Обязательное поле'),
+  password: string().min(6, 'Пароль слишком короткий').required('Обязательное поле'),
+  passwordConfirm: string()
+    .oneOf([yupRef('password'), null], 'Пароли должны совпадать!')
+    .required('Обязательное поле'),
+}).strict()
+
+const { handleSubmit } = useForm<InferType<typeof schema>>({
+  validationSchema: schema,
 })
 
-const handleSubmit = () => {
-  registerUser(registerData)
-}
+const { value: email, errorMessage: emailError } = useField<string>('email')
+const { value: password, errorMessage: passwordError } = useField<string>('password')
+const { value: passwordConfirm, errorMessage: passwordConfirmError } = useField<string>('passwordConfirm')
+
+const { push } = useRouter()
+
+const onSubmit = handleSubmit(async(values) => {
+  const registerResult = await registerUser({ ...values, username: 'username' })
+
+  if (registerResult.isRight())
+    push('/')
+})
 </script>
 <template>
-  <form class="flex flex-column align-items-center" @submit.prevent="handleSubmit">
-    <span class="p-float-label p-inputtext-lg mb-5 w-full">
-      <InputText id="email" v-model="registerData.email" class="w-full" type="text" />
-      <label for="email">Email</label>
-    </span>
-    <span class="p-float-label p-inputtext-lg mb-5 w-full">
-      <InputText id="password" v-model="registerData.password" class="w-full" type="password" />
-      <label for="password">Пароль</label>
-    </span>
-    <span class="p-float-label p-inputtext-lg mb-5 w-full">
-      <InputText
-        id="password-confirm"
-        v-model="registerData.passwordConfirm"
-        class="w-full"
-        type="password"
-      />
-      <label for="password">Подтвердите пароль</label>
-    </span>
-    <Button label="Зарегистироваться" class="p-button-rounded" type="submit" @click="handleSubmit" />
+  <form class="flex flex-column align-items-center h-full" @submit.prevent="onSubmit">
+    <div class="field w-full">
+      <span class="p-float-label p-inputtext-lg mb-4">
+        <InputText id="email" v-model="email" name="email" class="w-full" type="text" />
+        <div id="username2-help" class="p-error h-1rem text-sm">{{ emailError }}</div>
+        <label for="email">Email</label>
+      </span>
+      <span class="p-float-label p-inputtext-lg mb-4">
+        <InputText id="password" v-model="password" class="w-full" type="password" />
+        <div id="username2-help" class="p-error h-1rem text-sm">{{ passwordError }}</div>
+        <label for="password">Пароль</label>
+      </span>
+      <span class="p-float-label p-inputtext-lg">
+        <InputText
+          id="password-confirm"
+          v-model="passwordConfirm"
+          class="w-full"
+          type="password"
+        />
+        <div id="username2-help" class="p-error h-1rem text-sm">{{ passwordConfirmError }}</div>
+        <label for="password">Подтвердите пароль</label>
+      </span>
+    </div>
+    <div>
+      <Button label="Зарегистироваться" class="p-button-rounded" type="submit" />
+    </div>
   </form>
 </template>
