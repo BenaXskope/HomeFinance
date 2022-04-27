5ed9d477d37c7f25e455a7068e692b5f67393991
benaxskope <gyroskope225@gmail.com>
2022-04-28 00:08:57 +0300

    Имплементация с моком

---
+++ frontend/src/components/common/RecordTypeTag.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import Tag from 'primevue/tag'
+const props = defineProps<{
+  isExpense: boolean
+}>()
+
+const tagValue = computed(() => props.isExpense ? 'Расход' : 'Доход')
+const tagColor = computed(() => props.isExpense ? 'bg-primary' : 'bg-green-500')
+</script>
+<template>
+  <Tag :value="tagValue" class="w-5rem text-base" :class="[tagColor]" />
+</template>
--- frontend/src/views/NewRecordPage.vue
+++ frontend/src/views/NewRecordPage.vue
@@ -1,55 +1,234 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useField, useForm } from 'vee-validate'
-import type { InferType } from 'yup'
-import { number, object, string } from 'yup'
-import Button from 'primevue/button'
+import { boolean, number, object, string } from 'yup'
 import Dropdown from 'primevue/dropdown'
 import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
 import RadioButton from 'primevue/radiobutton'
+
+import Button from 'primevue/button'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import RecordTypeTag from '@components/common/RecordTypeTag.vue'
+import CreateFastRecordModal from '@components/new-record/CreateFastRecordModal.vue'
 
 const schema = object({
   category: number().required('Обязательное поле'),
-  amount: string().required('Обязательное поле'),
-  type: number().required('Обязательное поле'),
+  amount: number().required('Обязательное поле'),
+  type: boolean().required('Обязательное поле'),
   description: string(),
 })
 
-const { handleSubmit } = useForm<InferType<typeof schema>>({
+const { handleSubmit } = useForm({
   validationSchema: schema,
+  initialValues: {
+    category: undefined,
+    amount: undefined,
+    type: false,
+    description: undefined,
+  },
 })
 
 const { value: category } = useField<number>('category')
-const { value: amount } = useField<string>('amount')
-const { value: type } = useField<string>('type')
+const { value: amount } = useField<number>('amount')
+const { value: type } = useField<boolean>('type')
 const { value: description } = useField<string>('description')
 
+const onSubmit = handleSubmit((values) => {
+  console.log(values)
+})
+
+const mockCategories = [
+  {
+    label: 'Еда',
+    id: 1,
+  },
+  {
+    label: 'Одежда',
+    id: 2,
+  },
+  {
+    label: 'Техника',
+    id: 3,
+  },
+  {
+    label: 'Здоровье',
+    id: 4,
+  },
+]
+
+const mockFastRecords = [
+  {
+    id: 1,
+    category: 1,
+    categoryLabel: 'Еда',
+    amount: 1000,
+    isExpense: true,
+  },
+  {
+    id: 2,
+    category: 2,
+    categoryLabel: 'Одежда',
+    amount: 2000,
+    isExpense: true,
+  },
+  {
+    id: 3,
+    category: 3,
+    categoryLabel: 'Техника',
+    amount: 30000,
+    isExpense: false,
+  },
+  {
+    id: 4,
+    category: 4,
+    categoryLabel: 'Здоровье',
+    amount: 760,
+    isExpense: true,
+  },
+  {
+    id: 5,
+    category: 1,
+    categoryLabel: 'Еда',
+    amount: 1000,
+    isExpense: true,
+  },
+  {
+    id: 6,
+    category: 2,
+    categoryLabel: 'Одежда',
+    amount: 2000,
+    isExpense: true,
+  },
+  {
+    id: 7,
+    category: 3,
+    categoryLabel: 'Техника',
+    amount: 30000,
+    isExpense: false,
+  },
+  {
+    id: 8,
+    category: 4,
+    categoryLabel: 'Здоровье',
+    amount: 760,
+    isExpense: true,
+  },
+  {
+    id: 9,
+    category: 1,
+    categoryLabel: 'Еда',
+    amount: 1000,
+    isExpense: true,
+  },
+  {
+    id: 10,
+    category: 2,
+    categoryLabel: 'Одежда',
+    amount: 2000,
+    isExpense: true,
+  },
+  {
+    id: 11,
+    category: 3,
+    categoryLabel: 'Техника',
+    amount: 30000,
+    isExpense: false,
+  },
+  {
+    id: 12,
+    category: 4,
+    categoryLabel: 'Здоровье',
+    amount: 760,
+    isExpense: true,
+  },
+]
+
+const handleFastRecordSelected = (selectedRecord: typeof mockFastRecords[number]) => {
+  category.value = selectedRecord.category
+  amount.value = selectedRecord.amount
+  type.value = selectedRecord.isExpense
+}
+
+const isNewFastRecordDialogOpen = ref(false)
 </script>
 
 <template>
-  <h1 class="text-primary-semi-dark text-3xl">
+  <h1 class="text-primary-semi-dark mb-7">
     Создание новой записи
   </h1>
-  <div class="flex">
-    <form class="col-6 flex flex-column">
-      <div>
-        <div>Выберите категорию</div>
-        <Dropdown id="category" v-model="category" :options="cities" option-label="category" name="category" placeholder="Категория" />
+  <div class="flex text-lg">
+    <form class="col-6 flex flex-column" @submit.prevent="onSubmit">
+      <div class="mb-5">
+        <div class="mb-5">
+          Выберите категорию
+        </div>
+        <Dropdown id="category" v-model="category" class="w-full" :options="mockCategories" option-label="label" option-value="id" name="category" placeholder="Категория" />
       </div>
-      <div>
-        <div>Введите сумму</div>
-        <InputText id="amount" v-model="amount" type="number" name="amount" />
+      <div class="mb-5">
+        <div class="mb-5">
+          Введите сумму
+        </div>
+        <InputNumber id="amount" v-model="amount" class="w-full" name="amount" placeholder="Сумма" />
       </div>
-      <div>
-        <RadioButton id="type" v-model="type" name="type" />
+      <div class="mb-5 flex align-items-center">
+        <div class="col-6">
+          <RadioButton id="income" v-model="type" class="mr-4" name="type" :value="false" />
+          <label for="income">Доход</label>
+        </div>
+        <div class="col-6">
+          <RadioButton id="expense" v-model="type" class="mr-4" name="type" :value="true" />
+          <label for="expense">Расход</label>
+        </div>
       </div>
-      <div>
-        <div>Введите описание</div>
-        <InputText id="description" v-model="description" type="text" name="description" />
+      <div class="mb-3">
+        <div class="mb-5">
+          Введите описание
+        </div>
+        <InputText id="description" v-model="description" class="w-full" type="text" name="description" placeholder="Опсиание" />
       </div>
-      <Button label="СОЗДАТЬ" class="p-button-rounded" type="submit" />
+      <Button label="СОЗДАТЬ" class="p-button-rounded align-self-start" type="submit" />
     </form>
-    <div>
-      <Button icon="pi pi-plus" class="p-button-rounded" />
+    <div class="col-6">
+      <div class="w-full flex justify-content-between align-items-center mb-2">
+        <div>Быстрая запись</div>
+        <Button label="Добавить" class="p-button-rounded -mt-1" @click="isNewFastRecordDialogOpen = !isNewFastRecordDialogOpen" />
+      </div>
+      <DataTable
+        :value="mockFastRecords" :paginator="true" class="p-datatable-customers" :rows="5"
+        selection-mode="single"
+        data-key="id"
+        row-hover @update:selection="handleFastRecordSelected"
+      >
+        <Column field="amount">
+          <template #header>
+            Сумма
+          </template>
+          <template #body="{data}">
+            {{ data.amount }}
+          </template>
+        </Column>
+        <Column field="category">
+          <template #header>
+            Категория
+          </template>
+          <template #body="{data}">
+            {{ data.categoryLabel }}
+          </template>
+        </Column>
+        <Column field="isExpense">
+          <template #header>
+            Тип
+          </template>
+          <template
+            #body="{data}"
+          >
+            <RecordTypeTag :is-expense="data.isExpense" />
+          </template>
+        </Column>
+      </DataTable>
     </div>
+    <CreateFastRecordModal v-model="isNewFastRecordDialogOpen" />
   </div>
 </template>
